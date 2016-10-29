8a0b13f514660c2f53f43065c6fb01a7a9b704ee
tomwilderspin <tomwilderspin@gmail.com>
2016-10-29 17:35:30 +0100

    moved to es6 for rewrite of handler

---
+++ functions/alexa/nextTrain.js
@@ -0,0 +1,7 @@
+
+
+export const run =  (event, context, callback) => {
+
+  callback(null, 'test');
+
+}
--- tests/alexa/nextTrain.test.js
+++ tests/alexa/nextTrain.test.js
@@ -4,6 +4,18 @@ import { run } from '../../functions/alexa/nextTrain.js';
 
 describe('alexa next train event handler', () => {
 
-  //todo write up test for handler...
+  it('accepts an event and executes the callback response',  () => {
+
+    const mockCallback = jest.fn();
+
+    const mockEvent = {
+      key: 'value',
+      anotherKey: 'value2'
+    };
+
+    const result =  run(mockEvent, {}, mockCallback);
+
+    expect(mockCallback.mock.calls[0][1]).toBe('test');
+  })
 
 });
